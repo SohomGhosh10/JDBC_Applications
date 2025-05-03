31cd7070256a0cbf0f8bd74aa5c295cc81d6cb67
Sohom Ghosh <sohomghosh06@gmail.com>
2025-05-03 10:02:01 +0530

    Initial Commit

---
+++ src/Update_Data_JDBC.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+
+public class Update_Data_JDBC {
+    public static void main(String[] args) throws Exception {
+        String url = "jdbc:postgresql://localhost:5432/Demo";
+        String uname = "postgres";
+        String pass = "0000";
+
+        String query = "update \"Players\" set pname = 'Shami' where pid = 10";
+        Connection con = DriverManager.getConnection(url , uname , pass);
+        System.out.println("Connection Established");
+        Statement st = con.createStatement();
+        boolean status = st.execute(query);
+        System.out.println(status);
+
+        con.close();
+        System.out.println("Connection Closed");
+    }
+}
